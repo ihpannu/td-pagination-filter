4864d2af7ced43ffbb4a849b170c883e7008dea4
iharmanpannu <iharmanpannu@gmail.com>
2018-02-04 11:01:56 -0800

    starting project 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,1 @@
 const pagination = document.getElementsByClassName(".student-list");
-console.log("====================================");
-console.log(pagination);
-console.log("====================================");
