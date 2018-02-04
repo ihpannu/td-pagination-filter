53aba49ffe6e2aa41012b976e24132ab4dc8da71
iharmanpannu <iharmanpannu@gmail.com>
2018-02-03 23:47:12 -0800

    checking file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-console.log("hello");
+const pagination = document.getElementsByClassName(".student-list");
+console.log("====================================");
+console.log(pagination);
+console.log("====================================");
