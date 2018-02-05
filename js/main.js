2f92dbd0c6d2a5769df6976a4400c100718cf330
iharmanpannu <iharmanpannu@gmail.com>
2018-02-05 01:05:03 -0800

    changes

--- js/main.js
+++ js/main.js
@@ -17,3 +17,21 @@ function showPage(pageNumber, studentList) {
     }
   }
 }
+
+function appendPageLinks(studentList) {
+  let pageNum = Math.ceil(studentList.length / 10);
+  let pageLink = '<div class="pagination"> <ul>';
+  for (let i = 0; i < pageNum + 1; i++) {
+    pageNum += '<li><a href="#"> ' + i + "</a></li>";
+  }
+  pageNum += "</ul></div>";
+
+  studentContainer[0].insertAdjacentHTML("afterend", pageLink);
+
+  pages[0].className = "active";
+  for (let b = 0; b < pageNum; b++  ) {
+      pages[b].addEventListener('click', e => {
+          showPage(this.innerHTML, studentList);
+      }
+  }
+}
