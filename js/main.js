3f7e75cd0d6c1e001b27463df352fd811d4d32b0
iharmanpannu <iharmanpannu@gmail.com>
2018-02-04 21:50:27 -0800

    added window load function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 const studentList = document.getElementsByClassName(".student-list");
 const students = document.getElementsByClassName(".student-item");
 const page = document.getElementsByClassName(".page");
+
+window.addEventListener("load", () => {
+  for (let i = 0; i < students.length; i++) {
+    if (i < 10) {
+      students.style.display = "block";
+    } else {
+      students.style.display = "none";
+    }
+  }
+});
