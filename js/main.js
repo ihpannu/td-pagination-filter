80f9772620784aa6cae43e9c66b1899ee8747e4a
iharmanpannu <iharmanpannu@gmail.com>
2018-02-04 16:59:42 -0800

    trying to hide students

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-const pagination = (document.getElementsByClassName(
-  ".student-list"
-).style.display =
-  "none");
+const page = document.querySelector(".page");
+const studentList = document.querySelector(".student-list");
+const eachStudent = document.querySelectorAll(".student-item");
+const studentDetails = document.querySelector(".student-details");
+const navlink = document.querySelector(".navlink");
+
+//Set the pages variable
+const currentPage = 1;
+let numPages = 0;
+const studentsPerPage = 4;
+let index;
+
+//Finding the number of students
+const numberOfStudents = () => {
+  const numberOfStudents = eachStudent.length;
+  return numberOfStudents;
+};
+
+//Hiding all the students
+const hideAll = () => {
+  for (let i = 0; i < numberOfStudents(); i++) {
+    eachStudent[i].style.display = "none";
+  }
+};
+
+// Display only the 10 first items on the page
+function showStudents(number) {
+  for (let i = 0; i < studentsPerPage; i++) {
+    index = number * studentsPerPage - studentsPerPage + i;
+    eachStudent[index].style.display = "block";
+    console.log(index);
+  }
+}
