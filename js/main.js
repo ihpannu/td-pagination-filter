db14665335b6c8b703eb88f79bbc1d4195b7d8eb
iharmanpannu <iharmanpannu@gmail.com>
2018-02-05 00:20:05 -0800

    added function

--- js/main.js
+++ js/main.js
@@ -9,4 +9,11 @@ const pages = document.getElementsByTagName("a");
 //   return hideAll;
 // };
 
-function showPage(pageNumber, studentList) {}
+function showPage(pageNumber, studentList) {
+  for (let i = 0; i < students.length; i++) {
+    students[i].style.display = "none";
+    if (i < pageNumber + 0 && i > pageNumber + 0 - 10) {
+      students[i].style.display = "block";
+    }
+  }
+}
