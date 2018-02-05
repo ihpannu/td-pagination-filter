3f1a134eeb4b16b339f1457bf438960711aec4c5
iharmanpannu <iharmanpannu@gmail.com>
2018-02-04 23:13:28 -0800

    added showPage function

--- js/main.js
+++ js/main.js
@@ -2,16 +2,21 @@ const studentList = document.getElementsByClassName(".student-list");
 const student = document.getElementsByClassName(".student-item");
 const pages = document.getElementsByTagName("a");
 
-window.addEventListener("load", () => {
-  for (let i = 0; i < studentList.length; i++) {
-    i < 10
-      ? (students[i].style.display = "block")
-      : (student[i].style.display = "none");
-  }
-});
+// window.addEventListener("load", () => {
+//   for (let i = 0; i < studentList.length; i++) {
+//     i < 10
+//       ? (students[i].style.display = "block")
+//       : (student[i].style.display = "none");
+//   }
+// });
 
 function showPage(students, pageNum) {
   for (let i = 0; i < students.length; i++) {
-    students[i].style.display = "block";
+    students[i].style.display = "none";
+    if (i < pageNum + 0 && i >= pageNum + 0 - 10) {
+      students[i].style.display = "block";
+    }
   }
 }
+
+function appendPageLinks() {}
