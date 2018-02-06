eeaa39f70b9a9dcbebd7cb1412399179decdd1ac
iharmanpannu <iharmanpannu@gmail.com>
2018-02-06 00:01:22 -0800

    hidden all the lists

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 const studentsContainer = document.getElementsByClassName("student-list");
-const students = document.getElementsByClassName("student-items");
+const students = document.getElementsByClassName("student-item");
+
+window.addEventListener("load", () => {
+  for (let i = 0; i < students.length; i++) {
+    i < 10
+      ? (students[i].style.display = "block")
+      : (students[i].style.display = "none");
+  }
+});
