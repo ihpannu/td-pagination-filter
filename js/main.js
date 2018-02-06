9cebb06bdc064ad05649c3d0a3839ed24f2ef1e4
iharmanpannu <iharmanpannu@gmail.com>
2018-02-05 21:30:04 -0800

    added complete function

--- js/main.js
+++ js/main.js
@@ -15,8 +15,31 @@ window.addEventListener("load", () => {
 function showPage(pageNumber, studentList) {
   for (let i = 0; i < students.length; i++) {
     students[i].style.display = "none";
-    if (i < pageNumber + 0 && i > pageNumber + 0 - 10) {
+    if (i < pageNumber + 0 && i >= pageNumber + 0 - 10) {
       students[i].style.display = "block";
     }
+  }
+}
+
+function appendPageLinks(studentList) {
+  let pageNum = Math.ceil(studentList.length / 10);
+  let pageLink = '<div class="pagination"><ul>';
+  for (let i = 1; i < pageNum + 1; i++) {
+    pageLink += `<li><a class="active" href="#">${i}</a></li>`;
+  }
+  pageLink += "</ul></div>";
+
+  studentContainer[0].insertAdjacentHTML("afterend", pageLink);
+
+  pages[0].className = "active";
+
+  for (let a = 0; a < pageNum; a++) {
+    pages[a].addEventListener("click", function(e) {
+      showPage(this.innerHTML, studentList);
+      for (let b = 0; b < pageNum; b++) {
+        pages[b].className = "";
+      }
+      this.className = "active";
+    });
   }
 }
